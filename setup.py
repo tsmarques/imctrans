e69e08360a723f07b0d282de58b21593c754bc14
Ricardo Martins <rasm@oceanscan-mst.com>
2017-10-31 12:46:39 +0000

    Improved setup script.

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
 from setuptools import setup, find_packages
 
 setup(name='imctrans',
-      version='0.1.1',
+      version='0.1.2',
       description='IMC Bindings Generator',
       url='http://github.com/oceanscan/imctrans',
       author='Ricardo Martins',
       author_email='rasm@oceanscan-mst.com',
       license='Apache Software License (http://www.apache.org/licenses/LICENSE-2.0)',
       classifiers=[
-          "Development Status :: 4 - Beta",
-          "Topic :: Software Development :: Code Generators",
-          "License :: OSI Approved :: Apache Software License"
-
+          'Development Status :: 4 - Beta',
+          'Topic :: Software Development :: Code Generators',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
       ],
+      python_requires='>=3.4',
+      zip_safe=False,
       packages=find_packages('.'),
       package_data={
           'imctrans.cpp': ['assets/src/IMC/Base/*.hpp', 'assets/src/IMC/Base/*.cpp'],
